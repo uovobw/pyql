489de95ecc1c737206e7bde1bdd88b41b50a75bb
Matteo Bigoi <bigoi.m@maxmara.it>
2013-07-03 12:52:02 +0200

    Testing query parsing using nltk

---
+++ ws/MySqlParser.py
@@ -0,0 +1,78 @@
+# System Imports
+import nltk
+import sys
+
+# Local Imports
+from DbSchema import DbSchema
+
+class MySQLParser(object):
+    def __init__(self, query, position):
+        """
+            Input:
+                query:      str
+                position:   int
+        """
+        self.query = query
+        self.position = position
+        self.index_char = query[position]
+        self.config = {
+            'DB_USER': 'root',
+            'DB_PASSWORD': 'toor',
+            'DB_HOST': 'localhost',
+            'DB_PORT': '3306',
+            'DB_NAME': 'maxmara'
+        }
+        self.query_tokens = nltk.word_tokenize(query)
+        self.schema = DbSchema(self.config['DB_USER'], self.config['DB_PASSWORD'], self.config['DB_HOST'],
+                    self.config['DB_PORT'], self.config['DB_NAME'])
+
+
+class MySQLSelectParser(MySQLParser):
+    def parse(self):
+        curr_word, prev_word = self.search_precendent_words()
+        tables = self.search_tables_names()
+        print tables
+
+    def search_tables_names(self):
+        return [x for x in self.schema.getTables() if x in self.query_tokens]
+
+    def search_precendent_words(self):
+        if not self.index_char:
+            sys.exit(1)
+
+        char = self.index_char
+        position = self.position
+        counter = 0
+        exiter = 0
+        prev_word_list = []
+        while True:
+            char = self.query[position]
+            if char == ' ':
+                counter += 1
+            if counter == 2:
+                break
+            prev_word_list.insert(0, char)
+            position -= 1
+
+        tmp_word = ''.join(prev_word_list).split(' ')
+        curr_word = ''
+        prev_word = ''
+        if tmp_word:
+            curr_word = tmp_word[1]
+            prev_word = tmp_word[0]
+
+        return curr_word, prev_word
+
+
+qq = """
+    select colonna1, colonna2 from prelievi_pf
+    where colonna1 = (select id from righe_prelievi_pf)
+    group by colonna2
+    having sum( colonna2 ) > 10
+"""
+
+#for i in xrange(128):
+i = 56
+parse = MySQLSelectParser(qq, i)
+parse.parse()
+
