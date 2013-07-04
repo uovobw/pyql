8307bf782aed0248c9fd6e4777e741892f1562ad
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-04 15:47:14 +0200

    As usual, turns out the _test_ was wrong :)

--- ws/tests/TestMySQLSelectParser.py
+++ ws/tests/TestMySQLSelectParser.py
@@ -15,14 +15,19 @@ class TestMySQLSelectParser(unittest.TestCase):
 
     @mock.patch("DbSchema.DbSchema._read_schema", lambda x: None)
     def _create_fake_parser_for_query(self, query, position):
-        return MySQLSelectParser(query, position, self.fakeConfig)
+        to_return = MySQLSelectParser(query, position, self.fakeConfig)
+        to_return.schema.schema = {"test" : ["a", "b"],
+                                     "table1" : ["col1", "col2"],
+                                     "table2" : ["id", "another"]
+                                    }
+        return to_return
 
     def test_parse(self):
         qSet = (
-            ('select * from test', ['test']),
-            ('select * col1, col2 from table1 where col1 = (select id from table2) group by col2 having sum(col2) > 10', ['table1', 'table2'])
+            ('select * from test', set(('test',))),
+            ('select col1, col2 from table1 where col1 = (select id from table2) group by col2 having sum(col2) > 10', set(('table1', 'table2')))
         )
         for each in qSet:
             mySqlSelectParser = self._create_fake_parser_for_query(each[0], 4)
-            assert (mySqlSelectParser.parse() == each[1])
+            assert (set(mySqlSelectParser.parse()) == each[1])
 
