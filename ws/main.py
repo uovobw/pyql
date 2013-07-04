201e9bbbc522e6de7457f646e63425a005c501d0
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-04 18:00:40 +0200

    pep8-ify main.py

--- ws/main.py
+++ ws/main.py
@@ -4,17 +4,16 @@ from flask.ext.basicauth import BasicAuth
 
 #local imports
 from DbSchema import DbSchema
-from MySqlWords import MySqlWords
 
 app = Flask(__name__)
 app.debug = True
 
 app.config.update(
-    DB_HOST = "127.0.0.1",
-    DB_USER = "root",
-    DB_PASSWORD = "hackme",
-    DB_NAME = "employees",
-    DB_PORT = "3306"
+    DB_HOST="127.0.0.1",
+    DB_USER="root",
+    DB_PASSWORD="hackme",
+    DB_NAME="employees",
+    DB_PORT="3306"
 )
 
 app.config['BASIC_AUTH_USERNAME'] = 'user'
@@ -22,10 +21,12 @@ app.config['BASIC_AUTH_PASSWORD'] = 'password'
 
 basic_auth = BasicAuth(app)
 
+
 @app.route('/')
 @basic_auth.required
 def root():
     return 'Hello'
+
 
 @app.route("/columns/<t>")
 @basic_auth.required
@@ -41,4 +42,3 @@ if __name__ == '__main__':
                       )
     app.config['SCHEMA'] = schema
     app.run()
-
